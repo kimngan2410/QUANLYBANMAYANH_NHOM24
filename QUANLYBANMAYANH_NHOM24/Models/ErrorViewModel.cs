bf0146bdf6fdbbed6b0a53f148110716e4dfee3f
ADMIN <lethikimngan.dn43@gmail.com>
2024-11-27 11:25:35 +0700

    Add project files.

---
+++ QUANLYBANMAYANH_NHOM24/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QUANLYBANMAYANH_NHOM24.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
